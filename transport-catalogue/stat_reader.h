2fac303e5826b5284ee554db98b524cc6b2a5dbf
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-04-08 03:48:09 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,22 @@
-// место для вашего кода
+#pragma once
+
+#include <istream>
+#include <string>
+
+#include "transport_catalogue.h"
+
+namespace catalogue {
+
+	namespace stat_reader {
+
+		std::string_view CutName(std::string_view unprocessed_request);
+
+		void PrintInfoBus(const BusInfo& bus);
+
+		void PrintInfoStop(const StopInfo& bus);
+
+		void SearchInfo(const TransportCatalogue& catalogue, std::istream& input);
+
+	}
+
+}
