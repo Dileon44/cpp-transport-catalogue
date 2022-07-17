4bd878ef21d43c71170c71a779d387f1b6561241
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-07-17 03:48:23 +0300

    Update domain.cpp

--- transport-catalogue/domain.cpp
+++ transport-catalogue/domain.cpp
@@ -5,12 +5,12 @@ namespace catalogue {
 
 	bool CompareBuses::operator()(const Bus* l, const Bus* r) const {
 		return std::lexicographical_compare(l->name.begin(), l->name.end(),
-											r->name.begin(), r->name.end());
+						    r->name.begin(), r->name.end());
 	}
 
 	bool CompareStop::operator()(const Stop* l, const Stop* r) const {
 		return std::lexicographical_compare(l->name.begin(), l->name.end(),
-											r->name.begin(), r->name.end());
+						    r->name.begin(), r->name.end());
 	}
 
 }
