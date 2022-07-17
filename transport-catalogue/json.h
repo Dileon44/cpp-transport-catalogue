d1ff39745c768ac1705858216411ebb7041f020e
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-07-17 04:02:02 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -10,11 +10,10 @@ namespace json {
     using namespace std::literals;
 
     class Node;
-    // Сохраните объявления Dict и Array без изменения
+    
     using Dict = std::map<std::string, Node>;
     using Array = std::vector<Node>;
 
-    // Контекст вывода, хранит ссылку на поток вывода и текущий отсуп
     struct PrintContext {
         std::ostream& out;
         int indent_step = 4;
@@ -30,33 +29,26 @@ namespace json {
             }
         }
 
-        // Возвращает новый контекст вывода с увеличенным смещением
         PrintContext Indented() const {
             return { out, indent_step, indent_step + indent };
         }
     };
 
-    // Шаблон, подходящий для вывода double и int
     template <typename Value>
     void PrintValue(const Value& value, const PrintContext& ctx) {
         ctx.out << value;
     }
 
-    // Перегрузка функции PrintValue для вывода значений null
     void PrintValue(std::nullptr_t, const PrintContext& ctx);
 
-    // Перегрузка функции PrintValue для вывода значений bool
     void PrintValue(bool value, const PrintContext& ctx);
 
-    // Перегрузка функции PrintValue для вывода значений Array
     void PrintValue(const Array& value, const PrintContext& ctx);
 
-    // Перегрузка функции PrintValue для вывода значений Dict
     void PrintValue(const Dict& value, const PrintContext& ctx);
 
     void PrintNode(const Node& node, std::ostream& out);
 
-    // Эта ошибка должна выбрасываться при ошибках парсинга JSON
     class ParsingError : public std::runtime_error {
     public:
         using runtime_error::runtime_error;
