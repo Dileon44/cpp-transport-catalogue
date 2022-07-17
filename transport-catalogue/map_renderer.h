ca9edad79f93aadbf6c6de528cb9032d2beffd36
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-07-17 03:55:42 +0300

    Update map_renderer.h

--- transport-catalogue/map_renderer.h
+++ transport-catalogue/map_renderer.h
@@ -38,12 +38,10 @@ namespace catalogue {
 
         class SphereProjector {
         public:
-            // points_begin и points_end задают начало и конец интервала элементов geo::Coordinates
             template <typename PointInputIt>
             SphereProjector(PointInputIt points_begin, PointInputIt points_end,
                 double max_width, double max_height, double padding);
 
-            // Проецирует широту и долготу в координаты внутри SVG-изображения
             svg::Point operator()(geo::Coordinates coords) const;
 
         private:
@@ -71,20 +69,18 @@ namespace catalogue {
                 HighlightBusesAndCoordinatesOfStops() const;
 
             svg::Document CreateVisualization(const BusesCoordinates& bus_geo_coords,
-                const SphereProjector proj) const;
+                                              const SphereProjector proj) const;
 
             svg::Document CreatePolylinesRoutes(const BusesCoordinates& bus_geo_coords,
                                                 const SphereProjector proj) const;
-
-            //template<typename Obj>
-            //void SetPathPropsAttributesPolyline(svg::PathProps<Obj>* polyline, size_t count);
 
             void SetPathPropsAttributesPolyline(svg::Polyline& polyline, size_t count) const;
 
             void CreateRouteNames(svg::Document& document, const SphereProjector proj) const;
 
-            std::pair<svg::Text, svg::Text> CreateBackgroundAndTitleForRoute(
-                svg::Point point, std::string_view bus_name, size_t count) const;
+            std::pair<svg::Text, svg::Text> CreateBackgroundAndTitleForRoute(svg::Point point
+                                                                             , std::string_view bus_name
+                                                                             , size_t count) const;
 
             void CreateStopIcons(svg::Document& document
                                 , const std::set<const Stop*, CompareStop>& stops
@@ -99,63 +95,43 @@ namespace catalogue {
         SphereProjector::SphereProjector(
             PointInputIt points_begin, PointInputIt points_end,
             double max_width, double max_height, double padding)
-            : padding_(padding) //
-        {
-            // Если точки поверхности сферы не заданы, вычислять нечего
-            if (points_begin == points_end) {
-                return;
+            : padding_(padding) {
+                if (points_begin == points_end) {
+                    return;
             }
 
-            // Находим точки с минимальной и максимальной долготой
             const auto [left_it, right_it] = std::minmax_element(
                 points_begin, points_end,
                 [](auto lhs, auto rhs) { return lhs.lng < rhs.lng; });
             min_lon_ = left_it->lng;
             const double max_lon = right_it->lng;
 
-            // Находим точки с минимальной и максимальной широтой
             const auto [bottom_it, top_it] = std::minmax_element(
                 points_begin, points_end,
                 [](auto lhs, auto rhs) { return lhs.lat < rhs.lat; });
             const double min_lat = bottom_it->lat;
             max_lat_ = top_it->lat;
 
-            // Вычисляем коэффициент масштабирования вдоль координаты x
             std::optional<double> width_zoom;
             if (!IsZero(max_lon - min_lon_)) {
                 width_zoom = (max_width - 2 * padding) / (max_lon - min_lon_);
             }
 
-            // Вычисляем коэффициент масштабирования вдоль координаты y
             std::optional<double> height_zoom;
             if (!IsZero(max_lat_ - min_lat)) {
                 height_zoom = (max_height - 2 * padding) / (max_lat_ - min_lat);
             }
 
             if (width_zoom && height_zoom) {
-                // Коэффициенты масштабирования по ширине и высоте ненулевые,
-                // берём минимальный из них
                 zoom_coeff_ = std::min(*width_zoom, *height_zoom);
             }
             else if (width_zoom) {
-                // Коэффициент масштабирования по ширине ненулевой, используем его
                 zoom_coeff_ = *width_zoom;
             }
             else if (height_zoom) {
-                // Коэффициент масштабирования по высоте ненулевой, используем его
                 zoom_coeff_ = *height_zoom;
             }
         }
-
-        //template<typename Obj>
-        //void MapRenderer::SetPathPropsAttributesPolyline(svg::PathProps<Obj>* object, size_t count) {
-        //    object->SetFillColor(svg::NoneColor)
-        //        .SetStrokeColor(render_settings_.color_palette
-        //            .at(count % render_settings_.color_palette.size()))
-        //        .SetStrokeWidth(render_settings_.line_width)
-        //        .SetStrokeLineCap(svg::StrokeLineCap::ROUND)
-        //        .SetStrokeLineJoin(svg::StrokeLineJoin::ROUND);
-        //}
 
     } // namespace renderer
 
