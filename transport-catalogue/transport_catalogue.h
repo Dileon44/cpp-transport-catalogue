8aae56782fdcacfa9d940828106a08acdc620c2f
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-07-17 03:43:00 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -8,39 +8,9 @@
 #include <set>
 #include <functional>
 
+#include "domain.h"
+
 namespace catalogue {
-
-	enum class TypeRoute {
-		CIRCLE,
-		DIRECT
-	};
-
-	struct Stop {
-		std::string name;
-		double latitude;
-		double longitude;
-	};
-
-	struct Bus {
-		TypeRoute type_route;
-		std::string name;
-		std::deque<const Stop*> stops;
-	};
-
-	struct BusInfo {
-		bool bus_is_existing = false;
-		std::string name;
-		size_t number_stops = 0;
-		size_t unique_stops = 0;
-		double length_route = 0;
-		double curvature = 0;
-	};
-
-	struct StopInfo {
-		bool to_exist = false;
-		std::string name;
-		std::set<std::string_view> buses;
-	};
 
 	class TransportCatalogue {
 
@@ -64,25 +34,24 @@ namespace catalogue {
 		std::unordered_map<std::string_view, const Stop*> stop_to_ptr_stop_;
 		std::deque<Bus> buses_;
 		std::unordered_map<std::string_view, const Bus*> bus_to_ptr_bus_;
-		std::unordered_map<const Stop*, std::set<std::string_view>> stop_to_buses_;
+
+		std::unordered_map<const Stop*, std::unordered_set<const Bus*>> stop_to_buses_;
 
 		class HasherDistance {
 		public:
 			size_t operator()(std::pair<const Stop*, const Stop*> key) const {
 				return hasher_(key.first) + hasher_(key.second) * 16;
 			}
-
 		private:
 			std::hash<const Stop*> hasher_;
 		};
 
-		std::unordered_map<std::pair<const Stop*, const Stop*>, size_t, HasherDistance> distances_;
+		std::unordered_map<std::pair<const Stop*, const Stop*>, 
+			size_t, HasherDistance> distances_;
 
 		size_t CalculateUniqueStops(std::string_view bus) const;
-
 		double CalculateFactLenghtRoute(std::string_view bus) const;
-
 		double CalculateGeoLenghtRoute(std::string_view bus) const;
 	};
 
-}
+} // namespace catalogue	
