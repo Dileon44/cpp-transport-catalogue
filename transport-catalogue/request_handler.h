a998a1b3b733f92b05addc9fd4f7ddb14792c9f3
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-07-20 21:09:30 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -6,22 +6,24 @@
 
 #include "domain.h"
 #include "transport_catalogue.h"
+#include "map_renderer.h"
 
 namespace catalogue {
 
     class RequestHandler {
     public:
-        RequestHandler(const TransportCatalogue& db);
+        RequestHandler(const TransportCatalogue& db, const renderer::MapRenderer& renderer);
 
         std::optional<BusInfo> GetBusStat(const std::string_view& bus_name) const;
 
         const std::unordered_set<const Bus*> GetBusesByStop(const std::string_view& stop_name) const;
 
-        //svg::Document RenderMap() const;
+        svg::Document RenderMap(const renderer::RenderSettings& render_settings,
+                                const std::set< const Bus*, CompareBuses >& buses) const;
 
     private:
         const TransportCatalogue& db_;
-        //const renderer::MapRenderer& renderer_;
+        const renderer::MapRenderer& renderer_;
     };
 
 } // namespace catalogueS
