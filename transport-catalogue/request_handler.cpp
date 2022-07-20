ffb2c77fdfc2c3b367d13bcb90baff26b5d31618
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-07-20 21:09:52 +0300

    Update request_handler.cpp

--- transport-catalogue/request_handler.cpp
+++ transport-catalogue/request_handler.cpp
@@ -4,7 +4,10 @@
 
 namespace catalogue {
 
-	RequestHandler::RequestHandler(const catalogue::TransportCatalogue& db) : db_(db) {}
+	RequestHandler::RequestHandler(const catalogue::TransportCatalogue& db,
+								   const renderer::MapRenderer& renderer)
+		: db_(db), renderer_(renderer) {
+	}
 
 	std::optional<BusInfo> RequestHandler::GetBusStat(
 		const std::string_view& bus_name) const {
@@ -18,6 +21,11 @@ namespace catalogue {
 		const std::unordered_set<const Bus*> a = db_.GetStopInfo(stop_name).buses;
 
 		return a;
+	}
+
+	svg::Document RequestHandler::RenderMap(const renderer::RenderSettings& render_settings,
+											const std::set< const Bus*, CompareBuses >& buses) const {
+		return renderer_.RenderMap(render_settings, buses);
 	}
 
 } // namespace catalogue
