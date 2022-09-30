801ffabdb90b6f8d5a236ab8ece3f74178dffab6
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-10-01 01:58:31 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,18 +1,36 @@
 #include "request_handler.h"
 #include "json_reader.h"
-#include "map_renderer.h"
+#include "transport_catalogue.h"
+
+#include "log_duration.h"
+#include <fstream>
 
 int main() {
+	LOG_DURATION("Time: ");
 
 	catalogue::TransportCatalogue catalogue;
 	catalogue::renderer::MapRenderer map_renderer;
-	catalogue::RequestHandler request_handler(catalogue);
+	catalogue::RequestHandler request_handler(catalogue, map_renderer);
 	catalogue::JSONReader json_reader(catalogue, request_handler, map_renderer);
 
-	json_reader.ReadJSON(std::cin);
-	json_reader.BuildDataBase();
-	json_reader.GenerateAnswer();
-	json_reader.PrintAnswer(std::cout);
-	
+	std::ifstream input("s10_final_opentest_3.json");
+	std::ofstream output("b.txt");
+	{
+		LOG_DURATION("ReadJSON: ");
+		json_reader.ReadJSON(input);
+	}
+	{
+		LOG_DURATION("BuildDataBase: ");
+		json_reader.BuildDataBase();
+	}
+	{
+		LOG_DURATION("GenerateAnswer: ");
+		json_reader.GenerateAnswer();
+	}
+	{
+		LOG_DURATION("PrintAnswer: ");
+		json_reader.PrintAnswer(output);
+	}
+
 	return 0;
 }
