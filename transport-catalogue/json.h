f005b3121639d70e93d29d66ccc9167bc5a43fb7
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2022-10-01 01:55:36 +0300

    Update json.h

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -3,114 +3,79 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <vector>
 #include <variant>
+#include <vector>
 
 namespace json {
-    using namespace std::literals;
 
     class Node;
-    
     using Dict = std::map<std::string, Node>;
     using Array = std::vector<Node>;
-
-    struct PrintContext {
-        std::ostream& out;
-        int indent_step = 4;
-        int indent = 0;
-
-        PrintContext(std::ostream& out);
-
-        PrintContext(std::ostream& out, int indent_step, int indent = 0);
-
-        void PrintIndent() const {
-            for (int i = 0; i < indent; ++i) {
-                out.put(' ');
-            }
-        }
-
-        PrintContext Indented() const {
-            return { out, indent_step, indent_step + indent };
-        }
-    };
-
-    template <typename Value>
-    void PrintValue(const Value& value, const PrintContext& ctx) {
-        ctx.out << value;
-    }
-
-    void PrintValue(std::nullptr_t, const PrintContext& ctx);
-
-    void PrintValue(bool value, const PrintContext& ctx);
-
-    void PrintValue(const Array& value, const PrintContext& ctx);
-
-    void PrintValue(const Dict& value, const PrintContext& ctx);
-
-    void PrintNode(const Node& node, std::ostream& out);
 
     class ParsingError : public std::runtime_error {
     public:
         using runtime_error::runtime_error;
     };
 
-    class Node {
+    class Node final
+        : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
     public:
-        using Value = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
-        Node() = default;
+        using variant::variant;
+        using Value = variant;
+
         Node(Value value);
-        template <typename Variable>
-        Node(Variable variable);
-
-
-        int AsInt() const;
-        bool AsBool() const;
-        double AsDouble() const;
-        const std::string& AsString() const;
-        const Array& AsArray() const;
-        const Dict& AsMap() const;
 
         bool IsInt() const;
-        bool IsDouble() const;
+        int AsInt() const;
+
         bool IsPureDouble() const;
+        bool IsDouble() const;
+        double AsDouble() const;
+
         bool IsBool() const;
-        bool IsString() const;
+        bool AsBool() const;
+
         bool IsNull() const;
+
         bool IsArray() const;
-        bool IsMap() const;
+        const Array& AsArray() const;
+
+        bool IsString() const;
+        const std::string& AsString() const;
+
+        bool IsDict() const;
+        const Dict& AsDict() const;
+
+        bool operator==(const Node& rhs) const;
 
         const Value& GetValue() const;
-
-        bool operator==(const Node& other) const;
-        bool operator!=(const Node& other) const;
-
-    private:
-        Value value_ = nullptr;
+        Value& GetNoConstValue();
     };
 
-    std::ostream& operator<<(std::ostream& out, const Node& node);
+    inline bool operator!=(const Node& lhs, const Node& rhs) {
+        return !(lhs == rhs);
+    }
 
     class Document {
     public:
         explicit Document();
         explicit Document(Node root);
-
         const Node& GetRoot() const;
-
-        bool operator==(const Document& other) const;
-        bool operator!=(const Document& other) const;
 
     private:
         Node root_;
     };
 
+    inline bool operator==(const Document& lhs, const Document& rhs) {
+        return lhs.GetRoot() == rhs.GetRoot();
+    }
+
+    inline bool operator!=(const Document& lhs, const Document& rhs) {
+        return !(lhs == rhs);
+    }
+
     Document Load(std::istream& input);
 
     void Print(const Document& doc, std::ostream& output);
-
-    template <typename Variable>
-    Node::Node(Variable variable) {
-        value_ = variable;
-    }
 
 }  // namespace json
