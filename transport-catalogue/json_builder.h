b800bd7675b7b4ed37a5cd02474d637b5d7cf369
Dmitriy Leonov <88231309+Dileon44@users.noreply.github.com>
2022-11-08 23:26:19 +0300

    Update json_builder.h

--- transport-catalogue/json_builder.h
+++ transport-catalogue/json_builder.h
@@ -5,110 +5,61 @@
 #include <string>
 #include <vector>
 
-
 namespace json {
 
 	class DictItemContext;
 	class KeyItemContext;
-	class ValueAfterKeyContext;
-	class ArrayContext;
-	class ValueArrayContext;
-	class ValueContext;
-	class Builder;
-	class Node;
-
-	class BuilderContext {
-	public:
-		BuilderContext() = default;
-		BuilderContext(Builder& builder);
-		BuilderContext(ValueAfterKeyContext value);
-		BuilderContext(ArrayContext array_context);
-		BuilderContext(ValueArrayContext value);
-
-		DictItemContext StartDict();
-		BuilderContext EndDict();
-		ArrayContext StartArray();
-		BuilderContext EndArray();
-		KeyItemContext Key(std::string key);
-		ValueContext Value(Node::Value value);
-		Node Build() const;
-	private:
-		Builder& builder_;
-	};
-
-	// Класс-родитель, от него далее наследуются ValueAfterKeyContext и ValueArrayContext
-	class ValueContext : public BuilderContext {
-	public:
-		ValueContext(BuilderContext builder_context)
-			: BuilderContext(std::move(builder_context)) {
-		}
-	};
-
-	// 1
-	class KeyItemContext : public BuilderContext {
-	public:
-		KeyItemContext(BuilderContext builder_context);
-		ValueAfterKeyContext Value(Node::Value value);
-		BuilderContext Key() = delete;
-		BuilderContext EndDict() = delete;
-		BuilderContext EndArray() = delete;
-		Node Build() const = delete;
-	};
-
-	// 2
-	class ValueAfterKeyContext : public BuilderContext {
-	public:
-		ValueAfterKeyContext(BuilderContext builder_context);
-		BuilderContext StartDict() = delete;
-		BuilderContext StartArray() = delete;
-		BuilderContext EndArray() = delete;
-		BuilderContext Value(Node::Value value) = delete;
-		Node Build() const = delete;
-	};
-
-	// 3
-	class DictItemContext : public BuilderContext {
-	public:
-		DictItemContext(BuilderContext builder_context);
-		BuilderContext StartDict() = delete;
-		BuilderContext StartArray() = delete;
-		BuilderContext EndArray() = delete;
-		BuilderContext Value(Node::Value value) = delete;
-		Node Build() const = delete;
-	};
-
-	// 4
-	class ArrayContext : public BuilderContext {
-	public:
-		ArrayContext(BuilderContext builder_context);
-		ValueArrayContext Value(Node::Value value);
-		BuilderContext Key() = delete;
-		BuilderContext EndDict() = delete;
-		Node Build() const = delete;
-	};
-
-	// 5
-	class ValueArrayContext : public BuilderContext {
-	public:
-		ValueArrayContext(BuilderContext builder_context);
-		ValueArrayContext Value(Node::Value value);
-		BuilderContext Key() = delete;
-		BuilderContext EndDict() = delete;
-		Node Build() const = delete;
-	};
+	class ArrayItemContext;
+	//class Builder;
 
 	class Builder {
 	public:
 		Builder() = default;
+
+		Builder& EndDict();
+		Builder& Value(Node::Value value);
+		Builder& EndArray();
+
 		DictItemContext StartDict();
-		Builder EndDict();
+		ArrayItemContext StartArray();
 		KeyItemContext Key(std::string key);
-		ValueContext Value(Node::Value value);
-		ArrayContext StartArray();
-		Builder EndArray();
+
 		Node Build() const;
 	private:
 		Node root_;
 		std::vector<Node*> nodes_stack_;
+	};
+
+	// 1
+	class KeyItemContext {
+	public:
+		KeyItemContext(Builder& builder);
+		DictItemContext Value(Node::Value value);
+		DictItemContext StartDict();
+		ArrayItemContext StartArray();
+	private:
+		Builder& builder_;
+	};
+
+	// 2
+	class DictItemContext {
+	public:
+		DictItemContext(Builder& builder);
+		KeyItemContext Key(std::string key);
+		Builder& EndDict();
+	private:
+		Builder& builder_;
+	};
+
+	// 3
+	class ArrayItemContext {
+	public:
+		ArrayItemContext(Builder& builder);
+		ArrayItemContext Value(Node::Value value);
+		DictItemContext StartDict();
+		ArrayItemContext StartArray();
+		Builder& EndArray();
+	private:
+		Builder& builder_;
 	};
 }
