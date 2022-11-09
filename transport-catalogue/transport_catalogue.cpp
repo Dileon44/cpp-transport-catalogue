865ed9c4ab5732cf3e310938d568bcecf18ea6ac
Dmitriy Leonov <88231309+Dileon44@users.noreply.github.com>
2022-11-10 00:59:18 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <string_view>
 
+
 namespace catalogue {
 
 	using InfoMain = std::vector<Stop>;
@@ -45,6 +46,17 @@ namespace catalogue {
 		return (stop_to_ptr_stop_.count(stop)) ? stop_to_ptr_stop_.at(stop) : nullptr;
 	}
 
+	void TransportCatalogue::SetDistance(std::string_view from, std::string_view to, size_t distance) {
+
+		if (!distances_.count({ FindStop(from), FindStop(to) })) { // возможно не нужное условие
+			distances_.insert({ {FindStop(from), FindStop(to)}, distance });
+		}
+	}
+
+	void TransportCatalogue::SetRoutingSettings(RoutingSettings routing_settings) {
+		routing_settings_ = std::move(routing_settings);
+	}
+
 	BusInfo TransportCatalogue::GetBusInfo(std::string_view bus) const {
 
 		BusInfo bus_info;
@@ -72,13 +84,6 @@ namespace catalogue {
 		return { false, std::string(stop), {} };
 	}
 
-	void TransportCatalogue::SetDistance(std::string_view from, std::string_view to, size_t distance) {
-
-		if (!distances_.count({ FindStop(from), FindStop(to) })) { // возможно не нужное условие
-			distances_.insert({ {FindStop(from), FindStop(to)}, distance });
-		}
-	}
-
 	size_t TransportCatalogue::GetDistance(std::string_view from, std::string_view to) const {
 
 		if (distances_.count({ FindStop(from), FindStop(to) })) {
@@ -86,6 +91,18 @@ namespace catalogue {
 		}
 
 		return distances_.at({ FindStop(to), FindStop(from) });
+	}
+
+	const Graph& TransportCatalogue::GetGraph() const {
+		return *graph_ptr_;
+	}
+
+	const PairVertexesId& TransportCatalogue::GetPairVertexesId(std::string stop_name) const {
+		return stop_to_pair_vertex_.at(FindStop(stop_name));
+	}
+
+	const EdgeInfo& TransportCatalogue::GetEdgeInfo(const EdgeId edge_id) const {
+		return edges_info_.at(edge_id);
 	}
 
 	class HasherUnique {
@@ -125,15 +142,66 @@ namespace catalogue {
 	double TransportCatalogue::CalculateGeoLenghtRoute(std::string_view bus) const {
 		double distance = 0;
 
-		geo::Coordinates from = { FindBus(bus)->stops[0]->latitude, FindBus(bus)->stops[0]->longitude };
+		geo::Coordinates from = { FindBus(bus)->stops[0]->coordinate.lat, FindBus(bus)->stops[0]->coordinate.lng };
 
 		for (size_t n = 1; n < FindBus(bus)->stops.size(); ++n) {
 			auto stop = FindBus(bus)->stops[n];
-			geo::Coordinates to{ stop->latitude, stop->longitude };
+			geo::Coordinates to{ stop->coordinate.lat, stop->coordinate.lng };
 			distance += ComputeDistance(from, to);
 			from = to;
 		}
 		return distance;
+	}
+
+	const std::unordered_set<const Bus*> TransportCatalogue::GetAllBuses() const {
+		std::unordered_set<const Bus*> buses;
+		for (const auto& [_, bus_ptr] : bus_to_ptr_bus_) {
+			buses.insert(bus_ptr);
+		}
+		return buses;
+	}
+
+	const std::unordered_set<const Stop*> TransportCatalogue::GetAllStops() const {
+		std::unordered_set<const Stop*> stops;
+		for (const auto& [_, stop_ptr] : stop_to_ptr_stop_) {
+			stops.insert(stop_ptr);
+		}
+		return stops;
+	}
+
+	void TransportCatalogue::BuildVertexes() {
+		size_t count_vertex = 0;
+		for (const auto [_, stop_ptr] : stop_to_ptr_stop_) {
+			stop_to_pair_vertex_.insert({ stop_ptr, PairVertexesId{ count_vertex++, count_vertex++ } });
+		}
+	}
+
+	void TransportCatalogue::BuildGraph() {
+		BuildVertexes();
+		
+		size_t number_all_stops = stop_to_pair_vertex_.size();
+		graph_ptr_ = std::make_unique<Graph>(Graph(2 * number_all_stops));
+		edges_info_.reserve(2 * number_all_stops);
+		
+		for (const auto& [stop, pair_vertexes] : stop_to_pair_vertex_) {
+			graph_ptr_->AddEdge(graph::Edge<double>{
+				pair_vertexes.begin_wait, pair_vertexes.end_wait, routing_settings_.bus_wait_time
+			});
+			edges_info_.push_back(EdgeWaitInfo{ stop, routing_settings_.bus_wait_time });
+		}
+
+		for (const auto [_, bus] : bus_to_ptr_bus_) {
+			if (bus->type_route == TypeRoute::CIRCLE) {
+				AddEdgeBusInfo(bus->stops.begin(), bus->stops.end(), bus);
+			}
+			else {
+				size_t half = (bus->stops.size() + 1) / 2;
+				auto middle_it = std::next(bus->stops.begin(), half);
+				auto rmiddle_it = std::next(bus->stops.rbegin(), half);
+				AddEdgeBusInfo(bus->stops.begin(), middle_it, bus);
+				AddEdgeBusInfo(rmiddle_it - 1, bus->stops.rend(), bus);
+			}
+		}
 	}
 
 } // namespace catalogue
