40dc73b408163c719186f26d4e441027becabf27
Dmitriy Leonov <88231309+Dileon44@users.noreply.github.com>
2022-11-10 01:09:40 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,36 +1,23 @@
-#include "request_handler.h"
+#include "json.h"
 #include "json_reader.h"
 #include "transport_catalogue.h"
-
-#include "log_duration.h"
-#include <fstream>
+#include "request_handler.h"
+#include "transport_router.h"
 
 int main() {
-	LOG_DURATION("Time: ");
-
 	catalogue::TransportCatalogue catalogue;
 	catalogue::renderer::MapRenderer map_renderer;
 	catalogue::RequestHandler request_handler(catalogue, map_renderer);
 	catalogue::JSONReader json_reader(catalogue, request_handler, map_renderer);
 
-	std::ifstream input("s10_final_opentest_3.json");
-	std::ofstream output("b.txt");
-	{
-		LOG_DURATION("ReadJSON: ");
-		json_reader.ReadJSON(input);
-	}
-	{
-		LOG_DURATION("BuildDataBase: ");
-		json_reader.BuildDataBase();
-	}
-	{
-		LOG_DURATION("GenerateAnswer: ");
-		json_reader.GenerateAnswer();
-	}
-	{
-		LOG_DURATION("PrintAnswer: ");
-		json_reader.PrintAnswer(output);
-	}
+	catalogue::Data data = std::move(json_reader.ReadJSON(std::cin));
 
+	json_reader.BuildDataBase(data);
+	catalogue.BuildGraph();
+	catalogue::TransportRouter transport_router(catalogue.GetGraph());
+
+	json::Document answers = std::move(json_reader.GenerateAnswer(transport_router, data.stat_requests));
+	json::Print(answers, std::cout);
+	
 	return 0;
 }
