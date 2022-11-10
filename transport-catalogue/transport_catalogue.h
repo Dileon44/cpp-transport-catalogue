2784fcc1ea54a7295d6c5ecc978ed318ad870390
Dmitriy Leonov <88231309+Dileon44@users.noreply.github.com>
2022-11-10 17:01:38 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -10,14 +10,9 @@
 #include <variant>
 
 #include "domain.h"
-#include "graph.h"
 
 namespace catalogue {
-	using Graph = graph::DirectedWeightedGraph<double>;
-	using EdgeInfo = std::variant<EdgeWaitInfo, EdgeBusInfo>;
-
 	class TransportCatalogue {
-
 		using InfoMain = std::vector<Stop>;
 		using InfoSecondary = std::unordered_map<std::string, std::string>;
 
@@ -28,7 +23,6 @@ namespace catalogue {
 		const Bus* FindBus(std::string_view bus) const;
 		const Stop* FindStop(std::string_view stop) const;
 
-		void SetRoutingSettings(RoutingSettings routing_settings);
 		void SetDistance(std::string_view from, std::string_view to, size_t distance);
 
 		BusInfo GetBusInfo(std::string_view bus) const;
@@ -36,11 +30,6 @@ namespace catalogue {
 		size_t GetDistance(std::string_view from, std::string_view to) const;
 		const std::unordered_set<const Bus*> GetAllBuses() const;
 		const std::unordered_set<const Stop*> GetAllStops() const;
-		const Graph& GetGraph() const;
-		const PairVertexesId& GetPairVertexesId(std::string stop_name) const;
-		const EdgeInfo& GetEdgeInfo(const EdgeId edge_id) const;
-
-		void BuildGraph();
 
 	private:
 		std::deque<Stop> stops_;
@@ -60,40 +49,9 @@ namespace catalogue {
 
 		std::unordered_map<std::pair<const Stop*, const Stop*>,	size_t, HasherDistance> distances_;
 
-		RoutingSettings routing_settings_;
-		std::unordered_map<const Stop*, PairVertexesId> stop_to_pair_vertex_;
-		std::unique_ptr<Graph> graph_ptr_;
-		std::vector<EdgeInfo> edges_info_;
-
 		size_t CalculateUniqueStops(std::string_view bus) const;
 		double CalculateFactLenghtRoute(std::string_view bus) const;
 		double CalculateGeoLenghtRoute(std::string_view bus) const;
-
-		void BuildVertexes();
-
-		template<typename Iter>
-		void AddEdgeBusInfo(Iter first, Iter last, const Bus* bus);
 	};
-
-	template<typename Iter>
-	void TransportCatalogue::AddEdgeBusInfo(Iter first, Iter last, const Bus* bus) {
-
-		for (auto from = first; from != last; ++from) {
-			size_t distance = 0;
-			size_t span_count = 0;
-			VertexId from_vertex = stop_to_pair_vertex_.at(*from).end_wait;
-			for (auto to = std::next(from); to != last; ++to) {
-				auto before_to = std::prev(to);
-				distance += GetDistance((*before_to)->name, (*to)->name);
-				++span_count;
-
-				VertexId to_vertex = stop_to_pair_vertex_.at(*to).begin_wait;
-				double weight = double(distance) / routing_settings_.bus_velocity;
-
-				graph_ptr_->AddEdge(graph::Edge<double>{ from_vertex, to_vertex, weight });
-				edges_info_.push_back(EdgeBusInfo{ bus, weight, span_count });
-			}
-		}
-	}
 
 } // namespace catalogue
